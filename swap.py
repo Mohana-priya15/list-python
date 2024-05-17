8bbb994eb99cd77d0c44a53b16d956362c11ef3b
Mohana-priya15 <mpmsc1415@gmail.com>
2024-05-17 20:31:26 +0530

    Initial Commit

---
+++ swap.py
@@ -0,0 +1,9 @@
+m=([23,24,25,26,27])
+print("List value before change",m)
+print("Enter the index to change:")
+index=int(input())
+print("Enter the value:")
+val=int(input())
+m[index]=val
+
+print("List value after change:",m)
